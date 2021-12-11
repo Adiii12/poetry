7fcdde880d8c53b8b1a48abaf08530fc2be266da
Adil <marwatadil@yahoo.com>
2021-12-11 13:32:22 +0500

    new poetry website

---
+++ js/app.js
@@ -0,0 +1,25 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+function showMenu() {
+  document.getElementById("sideMenu").style.marginLeft = "0%";
+  document.getElementById("sideMenu").style.width = "70%";
+}
+
+function hideMenu() {
+  document.getElementById("sideMenu").style.marginLeft = "-70%";
+}
